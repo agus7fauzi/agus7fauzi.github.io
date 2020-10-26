f47c82039839cc9145a31b619276fed9b5b24c13
agus7fauzi <agus7fauzi@gmail.com>
2020-10-26 10:56:16 +0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-afdeda4e63d1b2280bd1.js"
+    "url": "webpack-runtime-d4577c7fd6c31cd1803e.js"
   },
   {
     "url": "framework-dac95de3c301f57a114a.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "eeae2ca9b9d8690e1259c966077f86cd"
+    "revision": "8c9126f24e0f701afe705d833656c08d"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "58b5d2915b5599e83a4ad2c7db28054b"
+    "revision": "bd3863a11255e7bae9379181e7284ecd"
   },
   {
     "url": "polyfill-4b1e8b7e04c413f9811f.js"
