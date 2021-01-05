4b047a7152093aabba2b2fef0650b1723ad0ce9e
agus7fauzi <agus7fauzi@gmail.com>
2021-01-06 06:18:31 +0700

    development updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,22 +3,15 @@ import { getUserLangKey } from 'ptz-i18n';
 import { withPrefix } from 'gatsby-link';
 import { navigate } from 'gatsby';
 
-class RedirectIndex extends React.PureComponent {
-  constructor(args) {
-    super(args);
+function RedirectIndex() {
+  // Skip build, Browsers only
+  if (typeof window !== 'undefined') {
+    const langKey = getUserLangKey(['en', 'id'], 'en');
+    const homeUrl = withPrefix(`/${langKey}`);
 
-    // Skip build, Browsers only
-    if (typeof window !== 'undefined') {
-      const langKey = getUserLangKey(['en', 'id'], 'en');
-      const homeUrl = withPrefix(`/${langKey}`);
-
-      navigate(homeUrl, { replace: true });
-    }
+    navigate(homeUrl, { replace: true });
   }
-
-  render() {
-    return <div />;
-  }
+  return <div />;
 }
 
 export default RedirectIndex;
